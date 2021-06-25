6eca8547909e39eb40b15d56f8699ad94a75f2ed
raveit65 <mate@raveit.de>
2021-06-25 18:35:21 +0200

    update copyright to 2021

--- Mozo/MenuEditor.py
+++ Mozo/MenuEditor.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #   Mozo Menu Editor - Simple fd.o Compliant Menu Editor
 #   Copyright (C) 2006  Travis Watkins, Heinrich Wendel
+#   Copyright (C) 2012-2021 MATE Developers
 #
 #   This library is free software; you can redistribute it and/or
 #   modify it under the terms of the GNU Library General Public
